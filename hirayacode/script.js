869c07d47d0e81e8d70d9c2ea6c1a433487bc6f1
Chrsc <crisxdwars@gmail.com>
2025-09-09 13:30:37 +0800

    Delete hirayacode directory

--- hirayacode/script.js
+++
@@ -1,8 +0,0 @@
- document.addEventListener('DOMContentLoaded', function() {
-    const tiles = document.querySelectorAll('.tile');
-    tiles.forEach((tile, index) => {
-        setTimeout(() => {
-            tile.classList.add('visible');
-        }, 100 * (index + 1));
-    });
-})
